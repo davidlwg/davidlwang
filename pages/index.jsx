f18ad69cf8b3ad2f25326ec12563bc5eb93d8484
David Wang <david2000wang@gmail.com>
2023-01-20 03:31:40 -0500

    update blurb

--- pages/index.jsx
+++ pages/index.jsx
@@ -12,9 +12,9 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-          <p>I&apos;m currently a forward deployed SWE intern at <a href="https://perpetua.io/" target="_blank" rel="noreferrer">Perpetua</a>.</p>
-          <p>I am also studying in a <a href="https://uwaterloo.ca/future-students/programs/business-administration-computer-science-double-degree" target="_blank" rel="noreferrer">double degree</a> program in Computer Science at the University of Waterloo and Business Administration at Wilfrid Laurier University.</p>
-          <p> Previously, I did internships as a research analyst at <a href="https://www.theblock.co/" target="_blank" rel="noreferrer">The Block Crypto</a> and a SWE at a unicorn called <a href="https://www.ada.cx/" target="_blank" rel="noreferrer">Ada</a>.</p>
+          <p>I am currently studying in a <a href="https://uwaterloo.ca/future-students/programs/business-administration-computer-science-double-degree" target="_blank" rel="noreferrer">double degree</a> program in Computer Science at the University of Waterloo and Business Administration at Wilfrid Laurier University.</p>
+          <p>I&apos;m also looking for summer 2023 internships. Please reach out with any opportunities! </p>
+          <p> Previously, I was a forward deployed SWE intern at <a href="https://perpetua.io/" target="_blank" rel="noreferrer">Perpetua</a>. Before that, a research analyst at <a href="https://www.theblock.co/" target="_blank" rel="noreferrer">The Block Crypto</a> and a SWE at a unicorn called <a href="https://www.ada.cx/" target="_blank" rel="noreferrer">Ada</a>.</p>
       </section>
       {/* <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}><mark>Projects</mark></h2>
